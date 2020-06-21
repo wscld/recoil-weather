dc767ad78b2cfaf151312ede6999472c5c59ee9f
Wesley <weslcld@gmail.com>
2020-06-21 03:03:47 -0300

    Updates

---
+++ precache-manifest.af947d8f6a469310ff684a126be22d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812fcea054259d7f6bbba3d244f319c3",
+    "url": "/recoil-weather/index.html"
+  },
+  {
+    "revision": "b95e0d093466dedc0af6",
+    "url": "/recoil-weather/static/css/main.5c4c66fe.chunk.css"
+  },
+  {
+    "revision": "42bde1ae0fd7e4fa5fec",
+    "url": "/recoil-weather/static/js/2.e5deb6e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recoil-weather/static/js/2.e5deb6e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95e0d093466dedc0af6",
+    "url": "/recoil-weather/static/js/main.53066a4d.chunk.js"
+  },
+  {
+    "revision": "b923c8b39830959d6bac",
+    "url": "/recoil-weather/static/js/runtime-main.462fcd72.js"
+  }
+]);
